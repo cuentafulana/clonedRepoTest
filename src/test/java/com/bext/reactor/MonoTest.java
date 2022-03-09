2e26e1b74611d2215ee3093986196db4532a8484
jalbertomr <jalbertomr@yahoo.com>
2022-03-09 16:19:53 -0600

    setup test

---
+++ src/test/java/com/bext/reactor/MonoTest.java
@@ -0,0 +1,12 @@
+package com.bext.reactor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+    @Test
+    public void test() {
+        log.info("All setup!");
+    }
+}
