e919eb1645def80e2fe4f5ffa6a0bb26ea3962cf
jalbertomr <jalbertomr@yahoo.com>
2022-03-09 17:15:32 -0600

    Mono.just, subscribe(), log()

--- src/test/java/com/bext/reactor/MonoTest.java
+++ src/test/java/com/bext/reactor/MonoTest.java
@@ -2,11 +2,15 @@ package com.bext.reactor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 public class MonoTest {
     @Test
-    public void test() {
-        log.info("All setup!");
+    public void monoSubscriberTest() {
+        Mono<String> mono = Mono.just("MonoHasJustThis").log();
+        mono.subscribe();
+
+        log.info("Mono: {}", mono);
     }
 }
